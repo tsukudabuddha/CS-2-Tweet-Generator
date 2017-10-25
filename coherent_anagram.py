28b378521d5af9655b4487c8e7177e996a7fc577
tsukudabuddha <andrewtsukuda@gmail.com>
2017-10-24 21:21:15 -0700

    Fully functional build

--- coherent_anagram.py
+++ coherent_anagram.py
@@ -21,5 +21,14 @@ def pair(word):
 user_word = sys.argv[1]
 
 filter_list = list(filter(lambda x: pair(x) == pair(user_word), word_list))
-word_chooser = r.randint(0, len(filter_list) - 1)
-print(filter_list[word_chooser])
+# Delete original word from list if inside
+if user_word in filter_list:
+    filter_list.remove(user_word)
+if len(filter_list) != 0:
+    word_chooser = r.randint(0, len(filter_list) - 1)
+    print(filter_list[word_chooser])
+    print("Total anagrams: " + str(len(filter_list)))
+    print("Anagrams:")
+    print(filter_list)
+else:
+    print("No anagrams for this word")
