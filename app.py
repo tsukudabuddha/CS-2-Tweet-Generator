29effbb5b92b681f240859388511a5f49fb7e5bd
tsukudabuddha <andrewtsukuda@gmail.com>
2017-11-09 15:12:14 -0800

    Heroku

--- app.py
+++ app.py
@@ -7,6 +7,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    # # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
-    # app.config['TRAP_BAD_REQUEST_ERRORS'] = True
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
