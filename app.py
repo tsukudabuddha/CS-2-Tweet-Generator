699cdfc1a2fd476a30e55602f3e07a1c760c73df
tsukudabuddha <andrewtsukuda@gmail.com>
2017-11-09 15:55:18 -0800

    updated server

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import word_frequency
+import stochastic_sampling
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page_text():
+    histogram = word_frequency.histogram("joey_words.txt")
+    sentence = ""
+    for _ in range(0, 10):
+        sentence.append(stochastic_sampling.stochastic_sample(histogram))
+    return sentence
 
 
 if __name__ == '__main__':
